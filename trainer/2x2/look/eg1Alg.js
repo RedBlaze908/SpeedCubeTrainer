b31ef9dc7f3515164f212a14f4815ca456c3c3fb
RedBlaze908 <77117275+RedBlaze908@users.noreply.github.com>
2025-06-14 11:41:57 +0200

    Update eg1Alg.js

---
+++ trainer/2x2/look/eg1Alg.js
@@ -0,0 +1,48 @@
+const eg1A = [
+    "R U' R2 F' R F U R' F R",
+    "R2 U R2 F' R2 U R2 U' F2 U' R",
+    "R' F R2 U' R' U R U' R' F",
+    "F U' R U2 R' F2 R' F R",
+    "R' F' R F U R' F' R U R' F' R",
+    "R' F R U2 R U' R2 F2 R F'",
+
+    "R' F' R U' F' R' F R2 U R'",
+    "F R U' R' U R' F' R U F' R U R'",
+    "F' U2 F' U F2 R' F2 R U2",
+    "R U R' F R U' R' U2 R' F R",
+    "R U F' R U R2 U' R U R'",
+    "F' R U R' U' R U R2 F' R",
+
+    "R2 U R' U2 R' U R U' R B2 R2",
+    "R' F R2 U' R2 F R",
+    "R' U' R' F2 U F' R F'",
+    "R' F R F' R' F R U' R' F R F",
+    "F' U R' F2 R F R' F' R F",
+    "R' U' R' F2 U F2 U2 R' U2 R",
+
+    "R U' R2 F R2 U R' U' R U' R'",
+    "R' F R F' U R U' R' U' R' F' R F",
+    "F' U2 R U2 R' U2 F",
+    "R' F' R2 U R' F' R U R'",
+    "R U R' U' F R U' R' U R' F' R",
+    "F' U2 F' R U2 R' U' F",
+
+    "R U' R' F R U' R' F R U R' F'",
+    "R' F R U2 R' F2 R F' R' F R",
+    "R U R2 F' U R2 U R2 U' R",
+    "R' F2 R F' R' F R2 U R'",
+    "R U' R' U R U' R' U' F R U' R'",
+    "R' F R U' R' F R U F' R' F R",
+
+    "R' F' R F R' F2 R U2 R' F' R",
+    "R U' R' U R U' R' U2 F R U' R'",
+    "R U' R' F R U R' F' U' R U' R'",
+    "R U2 R U2 R U B2 R2",
+    "R' F R F' R' F R2 U' R'",
+    "R' U' R' F2 U F2 U' R' U R",
+
+    "R' F R2 U' R' F R U R' F'",
+    "R U' R' U' R' F2 R2 U R' U R U R'",
+    "F' R U2 R' F' U2 F R' F' R",
+    "F R' F' R U R' F' R F R' F' R"
+];
